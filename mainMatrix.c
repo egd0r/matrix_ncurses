8ef01d0bf264aaf52e4c9e6ed27adc383ab48ef3
egd0r <e.veigarianne@lancaster.ac.uk>
2022-01-21 21:56:19 +0000

    Don't know what I was thinking before, completed with ease. Imaging having a thread for each letter ('dead_emoji')

--- mainMatrix.c
+++ mainMatrix.c
@@ -1,116 +1,102 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<ncurses.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ncurses.h>
 
-//Linked list
-// 0 -> 1 -> 2 -> 3 -> 4 (arrIndex/columnRef)
-// ptrs to strings here
-// pointer to next element
+#define MAX_DY 7
+#define DENSITY 20
 
-typedef struct intArrMAS {
-    //We just need to store the pointer to the initialised array duh
-    int arrIndex;
-    char *arrList;
-    struct intArrMAS *next;
-} intArr_t;
+typedef struct CHAR_STRUCT {
+    //Opacity?
+    char ch;
+    int dy;
+    int x;
+    int y;
+    struct CHAR_STRUCT *next;
+    struct CHAR_STRUCT *prev;
+} char_n;
 
-void * keyListener (void *argf) {
-    usleep(10000);
-    char *cptr = (char *)argf;
-    *cptr = getch();
+int maxX, maxY;
+char_n *head;
 
-    return NULL;
+void add_linked(char_n **head, char_n *toAdd) {
+    char_n *temp=*head, *prev=NULL;
+    for (; temp; temp=temp->next) prev=temp;
+    if (!prev) *head = toAdd;
+    else {
+        prev->next = toAdd;
+        toAdd->prev = prev;
+    }
+}
+
+
+char_n *remove_linked(char_n *toRemove) {
+    char_n *ret=NULL;
+    if (toRemove) {
+        if (toRemove->prev) toRemove->prev->next=toRemove->next;
+        if (toRemove->next) {
+            toRemove->next->prev=toRemove->prev;
+            ret=toRemove->next;
+        }
+        return ret;
+    } 
+}
+
+char_n *create_letter() {
+    //Generate random letter between 33 and 126 inc
+    //Allocate space and return heaped
+    char_n *ret = (char_n *)malloc(sizeof(char_n));
+    ret->ch = (rand()% (126-33))+33;
+    ret->x = rand()%maxX; //obvs depends on screen size
+    ret->y = 0;
+    ret->dy = (rand()%MAX_DY)+2;
+    //enqueue(ret);
+    add_linked(&head, ret);
+
+    return ret;
+}
+
+void move_letters() {
+    char_n *temp = head, *next;
+    //Clear
+    clear();
+    for (; temp; temp=next) {
+        next=temp->next;
+        mvaddch(temp->y, temp->x, temp->ch);
+        temp->y+=temp->dy;
+        if (temp->y > maxY)
+            next=remove_linked(temp);
+    }
 }
 
 int main() {
-    intArr_t *head = NULL;
-    //Init
-    initscr();
-    int rows, cols;
-    getmaxyx(stdscr, rows, cols);
-    //One thread listening and one thread printing
-    pthread_t keyListenThread;
-    //Mallocing space for user input character
-    char *userC = (char *)malloc(sizeof(char)); 
+    srand(time(0));
+    int ch;
+    initscr();           /* Start curses mode        */
+    getmaxyx(stdscr, maxY, maxX);
+    raw();              /* Line buffering disabled  */
+    keypad(stdscr, false);       /* We get F1, F2 etc..      */
+    noecho();           /* Don't echo() while we do getch */
+    nodelay(stdscr, true);
+    curs_set(0);
 
-    //Screen initialisation
-    keypad(stdscr, TRUE);
-    
-    //Welcome text and instructions after initialisation
-    // -> Add later
-    //Listens for first character
-    
-    //Listen and print
-    intArr_t *temp = head;
-    int print_enable = 0;
-    for (int i=0; i<cols; i++) {
-        pthread_create(&keyListenThread, NULL, keyListener, userC);
-        //Allocate some memory for this string
-        char *mallocedSect = (char *)malloc(sizeof(char)*rows);
-        pthread_join(keyListenThread, NULL);
-        if (33 <= *userC <= 126) {
-
-            if (print_enable==0) print_enable=1;
-
-            char *mallocTemp = mallocedSect;
-            for (int q=0; q<rows; q++) {
-                //Store the qth int in the qth section of memory - deref - this works as expected
-                *mallocTemp = (char)*userC;
-                //Increment memory pointer
-                mvprintw(q, i+10, "%p", mallocTemp);
-                refresh();
-                mvprintw(q, i, "%c", *userC);
-                refresh();
-                mallocTemp++;
-            }
-            //We add pointer to linked list so it can be disposed of
-            intArr_t *newElem = (intArr_t *)malloc(sizeof(intArr_t));
-
-            newElem->arrIndex = i;
-            newElem->arrList = mallocedSect;
-            //Will just print mallocedSect
-            mvprintw(0,42,"%p", newElem->arrList);
-            
-            
-            if (!temp) {
-                temp=newElem;
-            } else {
-                temp->next=newElem;
-            }
-
-            /*
-            We can have a function to start the process of continuously
-            printing each letter of the array. We could do this by incrementing
-            set values being printed to the next pointer or continuously print
-            the array incrementing the index. 
-            */
-
-
-        } else {
-            printf("Invalid character\n");
-            //Make sure not to skip a column!
-            i--;
+    //Game loop
+    while (1) {
+        for (int i=0; i<DENSITY; i++) {
+            create_letter();
         }
-        //Sort print enable here, we can use temp here too to cut on runtime
+        usleep(30000);
+        move_letters();
+        char enter = getch();
+        if (enter=='q')
+            break;
+        refresh();
     }
-    //Now at a position where we have linked list, things should be printed now since we have sorted print_enable
-
-    //Wait... aren't all list printing processes going to have to be threaded? Is that even possible?
-    refresh();
-    /*
-    int q = 10;
-    for (; head; head=head->next) {
-       mvprintw(10, q++, "%p, index:%d\n", head->arrList, head->arrIndex);
-       refresh();
-    }
-    */
-    getch();
-    endwin();
 
     
+    endwin();           /* End curses mode         */
 
     return 0;
+
 }
